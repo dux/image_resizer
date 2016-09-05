50dbade7b3ccaf877d7fa4e8f7047f71de75a713
Dino Reic <rejotl@gmail.com>
2016-09-05 17:10:16 +0200

    master: Removed sinatra :) - pure rack app

--- app/app.rb
+++ app/app.rb
@@ -7,21 +7,78 @@ require 'openssl'
 
 [:resize_encoder, :image, :init].each { |lib| require_relative "./lib/#{lib}" }
 
-class Resizer < Sinatra::Base
-
+class Object
   def r(what=nil)
     raise StandardError, what
+  end
+end
+
+###
+
+class Resizer
+
+  attr_accessor :request, :response, :params
+
+  class << self
+    def call(env)
+      app = new(env)
+      app.router
+      app.deliver
+    end
+  end
+
+  ###
+
+  def initialize(env)
+    @request  = Rack::Request.new(env)
+    @response = Rack::Response.new
+  end
+
+  def router
+    root = @request.path.split('/')[1]
+    return get(:root) unless root
+
+    @params = @request.params.keys.inject({}){|h,k| h[k.to_sym] = @request.params[k]; h }
+    @md5 = Digest::MD5.hexdigest @params.to_json
+
+    if request.env['HTTP_IF_NONE_MATCH'] == @md5
+      response.status = 304
+      response.write 'not-modified'
+      return
+    end
+
+    root = 'resize' if  root[0, 1] == 'r'
+
+    get(root)
+  end
+
+  def deliver
+    return [400, {}, ['Error: No response body']] unless @response.body[0]
+    @response.status ||= 200
+    @response.finish
   end
 
   def is_local
     ['127.0.0.1','0.0.0.0'].index(request.ip)
   end
 
-  get "/" do
-    return File.read('./app/views/index.html')
+  def get(what)
+    method = "get_#{what}"
+    if respond_to?(method)
+      @response.write send(method)
+    else
+      @response.status = 400
+      @response.write "Request :#{what} is not supported"
+    end
   end
 
-  get '/pack' do
+  ### ROTUES
+
+  def get_root
+    File.read('./app/views/index.html')
+  end
+
+  def get_pack
     opts = [:width, :height, :crop, :image].inject({}) { |h, k| h[k] = params[k] if params[k]; h }
     secret = params[:secret] || '-'
 
@@ -38,7 +95,7 @@ class Resizer < Sinatra::Base
   # height: height of image
   # crop: crop area of image
   # unsafe: allow width, height and crop to be defined in params
-  get "/r*" do
+  def get_resize
     opts = {}
 
     # recieved packed string
@@ -69,28 +126,28 @@ class Resizer < Sinatra::Base
 
     img = Image.new(image, opts[:q].to_i)
 
-    response.headers['Content-type'] = "image/#{img.ext}"
-    response.headers['Cache-control'] = 'public, max-age=10000000, no-transform'
-    response.headers['ETag'] = Digest::MD5.hexdigest opts.to_json
-
     if resize_width > 0
       return 'Image to large' if resize_width > 1500
-      data = img.resize_width(resize_width)
+      file = img.resize_width(resize_width)
     elsif resize_height > 0
       return 'Image to large' if resize_height > 1500
-      data = img.resize_height(resize_height)
+      file = img.resize_height(resize_height)
     else
       gravity = params[:gravity].to_s.downcase
       gravity = 'North' if gravity.length == 0
-      data = img.crop(crop_size, gravity)
+      file = img.crop(crop_size, gravity)
     end
 
-    send_file(data, :disposition => 'inline')
+    data = File.read file
+
+    response.headers['Content-Type'] = 'image/jpg'
+    response.headers['Cache-Control'] = 'public, max-age=10000000, no-transform'
+    response.headers['ETag'] = @md5
+    response.headers['Connection'] = 'keep-alive'
+    response.headers['Content-Disposition'] = %[inline; filename="#{@md5}.jpg"]
+    response.headers['Connection'] = 'keep-alive'
+
+    data
   end
 
 end
-
-if __FILE__ == $0
-  Resizer.run!
-end
-
