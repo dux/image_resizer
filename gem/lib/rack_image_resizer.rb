609371e6941c20fcd14513bddcd87e4a92bf8dce
Dino Reic <rejotl@gmail.com>
2018-12-12 01:59:12 +0100

    master: updated gem methods

--- gem/lib/rack_image_resizer.rb
+++ gem/lib/rack_image_resizer.rb
@@ -6,30 +6,28 @@ require_relative 'string'
 module ::RackImageResizer
   extend self
 
-  def resizer_secret
-    @secret || ENV.fetch('RESIZER_SECRET')
-  end
-
-  def resizer_url
-    @url    || ENV.fetch('RESIZER_URL')
-  end
-
   def set name, value
-    raise 'not allowed' unless [:secret, :url].include?(name)
+    raise 'not allowed' unless [:secret, :url, :host].include?(name)
     instance_variable_set '@%s' % name, value
+  end
+
+  def get name
+    instance_variable_get('@%s' % name) || ENV.fetch('RESIZER_%s' % name.to_s.upcase)
   end
 
   ###
 
-  # "http://foo.jpg".resize_image({ s: "222x222", q: 80 }
-  # "http://foo.jpg".resize_image("^200x200")
-  # "http://foo.jpg".resize_image - expcts size as param
-  def get opts
+  def build opts
     opts[:i] ||= opts.delete(:image)
     opts[:s] ||= opts.delete(:size) if opts[:size]
 
     # return empty pixel unless self
     return "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" if opts[:i].to_s == ''
+
+    # add host as prefix to image if relative
+    opts[:i] = get(:host) + opts[:i] if opts[:i][0,1] == '/'
+
+    raise ArgumentError.new('Invalid URL, no https?:// found') unless opts[:i] =~ %r{^https?://}
 
     opts[:i] = opts[:i].sub(%r{^(\w+)://}, '')
     prefix = $1 == 'https' ? 's' : 'p'
@@ -41,7 +39,7 @@ module ::RackImageResizer
     data.push Base64.urlsafe_encode64(opts.to_json).gsub(/=*\s*$/, '')
 
     # add check
-    data.push Digest::SHA1.hexdigest(resizer_secret+data.first)[0,4]
+    data.push Digest::SHA1.hexdigest(get(:secret)+data.first)[0,4]
 
     # add extension
     ext = opts[:i].split('.').last.to_s.downcase
@@ -49,6 +47,6 @@ module ::RackImageResizer
     data.push '.%s' % ext
 
     # return full url
-    [resizer_url, data.join('')].join('/r/')
+    [get(:url), data.join('')].join('/r/')
   end
 end
--- gem/lib/string.rb
+++ gem/lib/string.rb
@@ -1,10 +1,14 @@
 unless ''.respond_to?(:resized)
   class ::String
+    # "http://foo.jpg".resized({ s: "222x222", q: 80 }
+    # "http://foo.jpg".resized("^200x200")
+    # "http://foo.jpg".resized_image - expcts size as param
     def resized opts=nil
-      opts ||= {}
+      opts   ||= {}
+      opts     = { s: opts } if opts.is_a?(String)
       opts[:i] = self
 
-      RackImageResizer.get opts
+      RackImageResizer.build opts
     end
   end
 end
