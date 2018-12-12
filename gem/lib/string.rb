96dc1c295e9854e52da9c08d29b825096b3b09fb
Dino Reic <rejotl@gmail.com>
2018-12-12 02:04:55 +0100

    master: added ability to add size as Integer (width)

--- gem/lib/string.rb
+++ gem/lib/string.rb
@@ -2,10 +2,11 @@ unless ''.respond_to?(:resized)
   class ::String
     # "http://foo.jpg".resized({ s: "222x222", q: 80 }
     # "http://foo.jpg".resized("^200x200")
+    # "http://foo.jpg".resized(200)
     # "http://foo.jpg".resized_image - expcts size as param
     def resized opts=nil
       opts   ||= {}
-      opts     = { s: opts } if opts.is_a?(String)
+      opts     = { s: opts } unless opts.is_a?(Hash)
       opts[:i] = self
 
       RackImageResizer.build opts
